e948f99445657b83ef17ae6e9344aca0ffe1f554
David83656 <davidechevarrieta2012@gmail.com>
2022-11-03 09:13:31 -0300

    cambios finales falta solo agregar los elementos en columna A

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,13 @@
 
 import Frames.DataFrame;
-import Global.ScannerController;
-import Helpers.OptionSelecctedHelper;
-import Helpers.PrintHelper;
+
 
 public class Main {
 
   public static void main(String[] args) {
-    int opt = 0;
-    
+
     DataFrame D1=new DataFrame();
     D1.setVisible(true);
-    
-    
-    PrintHelper.welcomeMessage();
-    do {
-      PrintHelper.showMenu();
-      opt = ScannerController.getScanner().nextInt();
-      OptionSelecctedHelper.optionSelectedController(opt);
-      PrintHelper.cls();
-    } while (opt != 7);
 
-    ScannerController.dispose();
   }
 }
